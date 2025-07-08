5661959dc281716ebd83d968ab748e267f33f28a
zzp <1062877405@qq.com>
2025-07-08 09:38:56 +0800

    限制头像的分辨率

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Storage;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -20,10 +22,19 @@ class UsersController extends Controller
         }
 
         public function update(UserRequest $request, User $user)
-            {
-                $user->update($request->all());
-                return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+        {
+            $data = $request->except('avatar');
+
+            if ($request->avatar) {
+                // 存储文件并获取相对路径（相对于storage/app/public）
+                $relativePath = Storage::disk('public')->putFile('avatars', $request->avatar);
+
+                // 仅保存相对路径到数据库（如 "avatars/abc123.jpg"）
+                $data['avatar'] = $relativePath;
             }
 
+            $user->update($data);
+            return redirect()->route('users.show', $user->id)->with('success', '更新成功！');
+        }
 
 }
